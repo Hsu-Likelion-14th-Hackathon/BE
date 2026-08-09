5567e132c55aa9c605b7fb6460a537c6658fccde
hardwoong <dew04300@naver.com>
2026-08-09 22:29:23 +0900

    feat: boardingpass 도메인 레포지토리 코드 구현

---
+++ src/main/java/com/boardingpass/be/domain/boardingpass/BoardingPassItemRepository.java
@@ -0,0 +1,6 @@
+package com.boardingpass.be.domain.boardingpass;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardingPassItemRepository extends JpaRepository<BoardingPassItem, Long> {
+}
