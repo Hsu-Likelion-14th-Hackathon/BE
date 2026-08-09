07492d7b3c8471929ad3685bc17c455007cefb26
hardwoong <dew04300@naver.com>
2026-08-09 22:33:40 +0900

    feat: 설문 문항 레포지토리 코드 구현

---
+++ src/main/java/com/boardingpass/be/domain/survey/SurveyOptionRepository.java
@@ -0,0 +1,6 @@
+package com.boardingpass.be.domain.survey;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SurveyOptionRepository extends JpaRepository<SurveyOption, Long> {
+}
