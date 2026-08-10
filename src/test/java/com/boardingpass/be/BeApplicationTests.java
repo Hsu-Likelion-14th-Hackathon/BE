f204b77c57086912f68018ae2e3f06b248c93c23
hee0543 <chaehee0543@gmail.com>
2026-08-10 18:13:23 +0900

    fix: KakaoAuthClient RestTemplate 의존성 주입 문제 해결

--- src/test/java/com/boardingpass/be/BeApplicationTests.java
+++ src/test/java/com/boardingpass/be/BeApplicationTests.java
@@ -1,11 +1,14 @@
 package com.boardingpass.be;
 
+import com.boardingpass.be.config.TestRestClientConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestRestClientConfig.class)
 class BeApplicationTests {
 
   @Test
