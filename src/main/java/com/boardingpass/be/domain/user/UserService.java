48de9cbe9c47bddcdf84ea0c19c979bd0d2e7717
hee0543 <chaehee0543@gmail.com>
2026-08-10 16:45:27 +0900

    feat: 내 정보 조회/수정 서비스(UserService) 구현

---
+++ src/main/java/com/boardingpass/be/domain/user/UserService.java
@@ -0,0 +1,40 @@
+package com.boardingpass.be.domain.user;
+
+import com.boardingpass.be.domain.user.dto.UserMeResponse;
+import com.boardingpass.be.domain.user.dto.UserUpdateRequest;
+import com.boardingpass.be.domain.user.dto.UserUpdateResponse;
+import com.boardingpass.be.global.apiPayload.code.status.ErrorStatus;
+import com.boardingpass.be.global.exception.GeneralException;
+import com.boardingpass.be.global.security.SecurityUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  @Transactional(readOnly = true)
+  public UserMeResponse getMe() {
+    User user = findCurrentUser();
+    return UserMeResponse.from(user);
+  }
+
+  @Transactional
+  public UserUpdateResponse updateMe(UserUpdateRequest request) {
+    User user = findCurrentUser();
+
+    NationalityValidator.validate(request.nationality());
+    user.completeProfile(request.name(), request.birthDate(), request.nationality());
+
+    return UserUpdateResponse.from(user);
+  }
+
+  private User findCurrentUser() {
+    Long userId = SecurityUtils.getCurrentUserId();
+    return userRepository.findById(userId)
+        .orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
+  }
+}
