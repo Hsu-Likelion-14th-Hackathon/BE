577b881936bcff86accbca1b45e007250fdeb59f
hee0543 <chaehee0543@gmail.com>
2026-08-12 00:14:50 +0900

    feat: 쇼핑백 담기/조회/삭제 컨트롤러 구현

---
+++ src/main/java/com/boardingpass/be/domain/bag/ShoppingBagItemController.java
@@ -0,0 +1,45 @@
+package com.boardingpass.be.domain.bag;
+
+import com.boardingpass.be.domain.bag.dto.ShoppingBagCreateRequest;
+import com.boardingpass.be.domain.bag.dto.ShoppingBagListResponse;
+import com.boardingpass.be.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "ShoppingBag", description = "쇼핑백 API")
+@RestController
+@RequestMapping("/shopping-bag")
+@RequiredArgsConstructor
+public class ShoppingBagItemController {
+
+  private final ShoppingBagItemService shoppingBagItemService;
+
+  @Operation(summary = "쇼핑백 담기")
+  @PostMapping
+  public ApiResponse<Void> addShoppingBagItem(@Valid @RequestBody ShoppingBagCreateRequest request) {
+    shoppingBagItemService.addShoppingBagItem(request);
+    return ApiResponse.onSuccess(null);
+  }
+
+  @Operation(summary = "쇼핑백 조회")
+  @GetMapping
+  public ApiResponse<ShoppingBagListResponse> getShoppingBag() {
+    return ApiResponse.onSuccess(shoppingBagItemService.getShoppingBag());
+  }
+
+  @Operation(summary = "쇼핑백 항목 삭제")
+  @DeleteMapping("/{shoppingBagItemId}")
+  public ApiResponse<Void> removeShoppingBagItem(@PathVariable Long shoppingBagItemId) {
+    shoppingBagItemService.removeShoppingBagItem(shoppingBagItemId);
+    return ApiResponse.onSuccess(null);
+  }
+}
