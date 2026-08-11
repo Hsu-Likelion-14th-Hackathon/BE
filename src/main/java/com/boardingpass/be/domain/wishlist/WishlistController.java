a578ba77f2842363db2c83b8c8bd246bcdff9558
hee0543 <chaehee0543@gmail.com>
2026-08-11 23:56:54 +0900

    feat: 위시리스트 담기/조회/삭제 컨트롤러 구현

---
+++ src/main/java/com/boardingpass/be/domain/wishlist/WishlistController.java
@@ -0,0 +1,45 @@
+package com.boardingpass.be.domain.wishlist;
+
+import com.boardingpass.be.domain.wishlist.dto.WishlistCreateRequest;
+import com.boardingpass.be.domain.wishlist.dto.WishlistListResponse;
+import com.boardingpass.be.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Wishlist", description = "위시리스트 API")
+@RestController
+@RequestMapping("/wishlist")
+@RequiredArgsConstructor
+public class WishlistController {
+
+  private final WishlistService wishlistService;
+
+  @Operation(summary = "위시리스트 담기")
+  @PostMapping
+  public ApiResponse<Void> addWishlist(@Valid @RequestBody WishlistCreateRequest request) {
+    wishlistService.addWishlist(request);
+    return ApiResponse.onSuccess(null);
+  }
+
+  @Operation(summary = "위시리스트 조회")
+  @GetMapping
+  public ApiResponse<WishlistListResponse> getWishlist() {
+    return ApiResponse.onSuccess(wishlistService.getWishlist());
+  }
+
+  @Operation(summary = "위시리스트 삭제")
+  @DeleteMapping("/{productColorId}")
+  public ApiResponse<Void> removeWishlist(@PathVariable Long productColorId) {
+    wishlistService.removeWishlist(productColorId);
+    return ApiResponse.onSuccess(null);
+  }
+}
