99e400414bbd2e435f74bb6d674c7f9b5e4cbba5
hee0543 <chaehee0543@gmail.com>
2026-08-12 18:03:13 +0900

    feat: 추가 정보 입력 응답에 여권번호 포함

--- src/main/java/com/boardingpass/be/domain/auth/dto/ProfileResponse.java
+++ src/main/java/com/boardingpass/be/domain/auth/dto/ProfileResponse.java
@@ -1,5 +1,6 @@
 package com.boardingpass.be.domain.auth.dto;
 
+import com.boardingpass.be.domain.passport.Passport;
 import com.boardingpass.be.domain.user.User;
 import java.time.LocalDate;
 
@@ -7,14 +8,16 @@ public record ProfileResponse(
     Long userId,
     String name,
     LocalDate birthDate,
-    String nationality
+    String nationality,
+    String passportNo
 ) {
-  public static ProfileResponse from(User user) {
+  public static ProfileResponse of(User user, Passport passport) {
     return new ProfileResponse(
         user.getId(),
         user.getName(),
         user.getBirthDate(),
-        user.getNationality()
+        user.getNationality(),
+        passport.getPassportNo()
     );
   }
 }
