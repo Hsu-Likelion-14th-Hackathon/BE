d01096b06e990e4d529d99519ac71f2c37b5dea4
hee0543 <chaehee0543@gmail.com>
2026-08-13 17:00:40 +0900

    feat: AuthController에 일반 회원가입/로그인 엔드포인트 추가

--- src/main/java/com/boardingpass/be/domain/auth/controller/AuthController.java
+++ src/main/java/com/boardingpass/be/domain/auth/controller/AuthController.java
@@ -2,8 +2,12 @@ package com.boardingpass.be.domain.auth.controller;
 
 import com.boardingpass.be.domain.auth.dto.KakaoLoginRequest;
 import com.boardingpass.be.domain.auth.dto.KakaoLoginResponse;
+import com.boardingpass.be.domain.auth.dto.LoginRequest;
+import com.boardingpass.be.domain.auth.dto.LoginResponse;
 import com.boardingpass.be.domain.auth.dto.ProfileRequest;
 import com.boardingpass.be.domain.auth.dto.ProfileResponse;
+import com.boardingpass.be.domain.auth.dto.SignupRequest;
+import com.boardingpass.be.domain.auth.dto.SignupResponse;
 import com.boardingpass.be.domain.auth.service.AuthService;
 import com.boardingpass.be.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,6 +31,18 @@ public class AuthController {
   @PostMapping("/kakao")
   public ApiResponse<KakaoLoginResponse> loginWithKakao(@Valid @RequestBody KakaoLoginRequest request) {
     return ApiResponse.onSuccess(authService.loginWithKakao(request));
+  }
+
+  @Operation(summary = "일반 회원가입")
+  @PostMapping("/signup")
+  public ApiResponse<SignupResponse> signup(@Valid @RequestBody SignupRequest request) {
+    return ApiResponse.onSuccess(authService.signup(request));
+  }
+
+  @Operation(summary = "일반 로그인")
+  @PostMapping("/login")
+  public ApiResponse<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+    return ApiResponse.onSuccess(authService.login(request));
   }
 
   @Operation(summary = "추가 정보 입력")
